c908aaf06c4ed100183630d0dc1c3a40a941719d
shreedhar4 <65651536+shreedhar4@users.noreply.github.com>
2020-08-08 21:21:46 +0530

    Update nav.js

--- site/js/nav.js
+++ site/js/nav.js
@@ -7,3 +7,18 @@ $(function () {
         }
     });
 });
+
+   var age= prompt("this is age restricted content please confirme your age to continue")
+    if(age>105){
+     alert("bitch you lying")}
+  
+ var feelings= prompt("how are you feeling- ans in true and false , true being positive feeings and false being negative")
+                      if(feelings==="false"){
+   alert("dont worry everything is going to be fine soon and if you need someone to vent to hmu, do enjoy my test page")
+ }else{ if(feelings==="true"){
+   alert("thats good you have a duty towards urself to feel good everyday of your life, do enjoy my test page")
+ }else {
+   alert("please ans in true and false")
+   
+ }
+ }
